47f40f11bef2e9248e4feb48fc62284a74efc47e
Marco Fuentes <marcofuentes1607@gmail.com>
2019-02-14 19:02:20 -0600

    Primer Commit

---
+++ app/src/main/java/com/example/fuent/laboratorio6/Song.kt
@@ -0,0 +1,18 @@
+package com.example.fuent.laboratorio6
+
+/**
+ * Created by Marco Fuentes on 14/02/2019.
+ */
+
+class Song {
+    private var id : Long =0L
+    private var title : String =" "
+    private var artist : String = ""
+
+    constructor(i : Long, ti : String, ar : String){
+        id = i
+        title = ti
+        artist = ar
+    }
+
+}
